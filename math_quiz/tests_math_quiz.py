347053ba220efa87c5c7917734af65b2e50b81d4
Vivien Correa <viviencorrea@viviens-mbp.localdomain>
2024-11-04 15:05:41 +0100

    Created and completed the unit tests for the functions of the math_quiz

--- math_quiz/tests_math_quiz.py
+++ math_quiz/tests_math_quiz.py
@@ -1,10 +1,10 @@
 import unittest
-from math_quiz import function_A, function_B, function_C
+from math_quiz import generate_random_integer, random_arithmetic_operation, arithmetic_operation
 
 
 class TestMathGame(unittest.TestCase):
 
-    def test_function_A(self):
+    def test_generate_random_integer(self):
         # Test if random numbers generated are within the specified range
         min_val = 1
         max_val = 10
@@ -12,19 +12,31 @@ class TestMathGame(unittest.TestCase):
             rand_num = function_A(min_val, max_val)
             self.assertTrue(min_val <= rand_num <= max_val)
 
-    def test_function_B(self):
-        # TODO
-        pass
+    def test_random_arithmetic_operation(self):
+        """ Test if the function returns a valid arithmetic operation """
+        valid_operators = ['+', '-', '*']
+        
+        for _ in range(1000):  # Test with a large number multiple times for randomness
+            operator = get_random_operator()
+            self.assertIn(operator, valid_operators)
+        
 
-    def test_function_C(self):
-            test_cases = [
-                (5, 2, '+', '5 + 2', 7),
-                ''' TODO add more test cases here '''
+    def test_arithmetic_operation(self):
+            """Test if the function can correctly create math problems and compute the answers."""
+        test_cases = [
+            (5, 2, '+', '5 + 2', 7),
+            (8, 3, '-', '8 - 3', 5),
+            (4, 6, '*', '4 * 6', 24),
+            (1, 1, '+', '1 + 1', 2),
+            (6, 2, '*', '6 * 2', 12)
+            
             ]
 
-            for num1, num2, operator, expected_problem, expected_answer in test_cases:
-                # TODO
-                pass
+            for number1, number2, operator, expected_problem, expected_answer in test_cases:
+                problem, answer = arithmetic_operation(number1, number2, operation)
+                self.assertEqual(problem, expected_problem)
+                self.assertEqual(answer, expected_answer)
+                
 
 if __name__ == "__main__":
     unittest.main()
